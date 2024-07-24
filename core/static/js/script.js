120f66d6329f271690615d2ef553efd627150755
Nycollas <nhhenich@gmail.com>
2024-07-24 00:19:10 -0300

    atualizando templates

---
+++ core/static/js/script.js
@@ -0,0 +1,29 @@
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 1,
+    spaceBetween: 8,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      "@0.00": {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      "@0.75": {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      "@1.00": {
+        slidesPerView: 3,
+        spaceBetween: 40,
+      },
+      "@1.50": {
+        slidesPerView: 4,
+        spaceBetween: 50,
+      },
+    },
+  });
---
+++ core/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('index/', IndexView.as_view(), name='index'),
+]
