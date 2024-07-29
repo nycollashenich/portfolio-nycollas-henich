548b901d32c18c6b14d7a46dfaa99e0227493d45
Nycollas <nhhenich@gmail.com>
2024-07-29 12:42:49 -0300

    .

--- setup/settings.py
+++ setup/settings.py
@@ -151,3 +151,11 @@ else:
     EMAIL_USE_TLS = config('EMAIL_USE_TLS')
     EMAIL_PORT = config('EMAIL_PORT')
     EMAIL_HOST = config('EMAIL_HOST')
+
+  
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST = config('EMAIL_HOST')
