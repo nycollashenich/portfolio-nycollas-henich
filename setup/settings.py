ea376d467b44c6ca9b4329f99962df27bb75766d
Nycollas <nhhenich@gmail.com>
2024-07-29 12:00:01 -0300

    .

--- setup/settings.py
+++ setup/settings.py
@@ -129,10 +129,10 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
