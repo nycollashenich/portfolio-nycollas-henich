db5054503c0e496ffd6cb0f3112272dc29f321e8
Nycollas <nhhenich@gmail.com>
2024-07-29 17:55:06 -0300

    .

--- setup/settings.py
+++ setup/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://portfolio-nycollas-henich-production.up.railway.app/']
+ALLOWED_HOSTS = ['*']
 
 
 
